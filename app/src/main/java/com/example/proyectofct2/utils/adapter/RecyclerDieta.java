30eb9aef9d672ada522a708fb9891aa0d96cd140
pris1998 <95096307+pris1998@users.noreply.github.com>
2023-06-09 15:54:17 +0200

    Terminado

--- app/src/main/java/com/example/proyectofct2/utils/adapter/RecyclerDieta.java
+++ app/src/main/java/com/example/proyectofct2/utils/adapter/RecyclerDieta.java
@@ -1,7 +1,5 @@
 package com.example.proyectofct2.utils.adapter;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
